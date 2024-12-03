3a447b96dfb34990a663d45185725a945ce5b3ea
csuhj <csuhj@users.noreply.github.com>
2024-12-03 21:11:17 +0000

    Day 3 part 1

---
+++ Day3/Part1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+using Utilities;
+
+const string Url = "2024/day/3/input";
+const string CookieFilePath = "../../session-cookie.txt";
+
+string contents = await DownloadHelper.DownloadInput(CookieFilePath, Url);
+
+Regex regex = new Regex("mul\\((\\d+),(\\d+)\\)");
+var matches = regex.Matches(contents);
+int sumOfProducts = matches.Aggregate(0, (sum, match) => sum + (int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value)));
+
+Console.WriteLine($"Sum of {matches.Count} mul products is {sumOfProducts}");
